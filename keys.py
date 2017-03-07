680e43bd6f465cc38caf846adb60897f91d59450
Nagendra Posani <nagendrachowdaryp@gmail.com>
2017-03-06 19:53:08 -0500

    removed keys

--- keys.py
+++ keys.py
@@ -3,8 +3,8 @@
 # replace the words in caps with the keys that we saw before on twitter
 # leave the quotes
 keys = {
-    'consumer_key': 'yjk9TubDktpgZVKRkyUwcgmFX',
-    'consumer_secret': 'du3JABOdhkmSKZVTtsYgdmFvpQwu5vfWYXuQ08fRrggZTaJadY',
-    'access_token': '561161760-JN3vo1VUUgQ7MLkzOrNJCt3kBQuKNIwQnGAf5JSF',
-    'access_token_secret': 'g86f3cllFmxQrywVn5kv2V1HSQKx262ryzXOGzqXtjS7B',
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': '',
 }
