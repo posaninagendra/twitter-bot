8baaaf7be147ada7572c58e9008ba6501885960c
Nagendra Posani <nagendrachowdaryp@gmail.com>
2017-03-06 19:41:08 -0500

    added code

--- bot.py
+++ bot.py
@@ -13,3 +13,18 @@ ACCESS_TOKEN_SECRET = keys['access_token_secret']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
+query = "baahubali 2"
+
+tweet_lists = api.search(q=query, count=20, lang="en")
+
+for tweet in tweet_lists:
+	screenname = tweet.user.screen_name
+	message = ".@{username} {message}".format(username=screenname, message='Awaiting the sequel of magnumopus creation of @ssrajamouli #baahubali2 #wkkb')
+	try:
+		api.update_status(status=message, in_reply_to_status_id=tweet.id)
+		print message
+	except tweepy.TweepError as e:
+		print e.message[0]['code']
+		print e.args[0][0]['code']
+
