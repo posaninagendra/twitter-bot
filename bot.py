f8583c65a1c90d9baacd55462ba0a56017fdd058
Nagendra Posani <nagendrachowdaryp@gmail.com>
2017-03-06 19:14:58 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import tweepy
+
+# from our keys module (keys.py), import the keys dictionary
+from keys import keys
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
